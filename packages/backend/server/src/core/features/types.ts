9137bb28c3b995d905e83abf14abaa886aec81cf
Arham Babar <Arhambabar24@gmail.com>
2026-06-04 15:21:57 +0200

    Revert "ci: lowercase docker tag"
    This reverts commit 6c2e39a742063cd9f81ed5af449cf8c992511b1f.

---
+++ packages/backend/server/src/core/features/types.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+
+import { Feature, UserFeatureName } from '../../models';
+
+@Injectable()
+export class AvailableUserFeatureConfig {
+  availableUserFeatures(): Set<UserFeatureName> {
+    return new Set([Feature.Admin, Feature.EarlyAccess, Feature.AIEarlyAccess]);
+  }
+
+  configurableUserFeatures(): Set<UserFeatureName> {
+    return new Set(
+      env.selfhosted
+        ? [Feature.Admin]
+        : [Feature.EarlyAccess, Feature.AIEarlyAccess, Feature.Admin]
+    );
+  }
+}
